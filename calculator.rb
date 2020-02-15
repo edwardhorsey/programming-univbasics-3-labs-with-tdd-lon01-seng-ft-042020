56d741eade4a5ad54ba9bb3dbc8108098df20676
Edward Horsey <edwardhorsey@me.com>
2020-02-15 17:06:49 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,5 +1,10 @@
 # Add your variables here
 
+
+first_variable = 3.5 
+second_variable = 5.5
+mode = add
+
 def calculator(first_variable, second_variable, mode)
   when mode == add
     puts first_variable + second_variable
