d84f837290e82c854d3ccf63cdbf88b3c6e6f735
Edward Horsey <edwardhorsey@me.com>
2020-02-15 17:08:13 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -5,16 +5,16 @@ first_variable = 3.5
 second_variable = 5.5
 mode = add
 
-def calculator(first_variable, second_variable, mode)
-  when mode == "add"
-    puts first_variable + second_variable
-    when mode == "subtract"
-      puts first_variable - second_variable
-      when mode == "multiply" 
-        puts first_variable * second_variable
-        when mode == "divide"
-          puts first_variable / second_variable
-        else null
-      end
+# def calculator(first_variable, second_variable, mode)
+#   when mode == "add"
+#     puts first_variable + second_variable
+#     when mode == "subtract"
+#       puts first_variable - second_variable
+#       when mode == "multiply" 
+#         puts first_variable * second_variable
+#         when mode == "divide"
+#           puts first_variable / second_variable
+#         else null
+#       end
       
-calculator(2, 3, subtract)
+# calculator(2, 3, subtract)
