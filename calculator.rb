84b202dd1f97b982ceeac054a2c69b32c275f651
Edward Horsey <edwardhorsey@me.com>
2020-02-15 17:03:42 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,13 @@
 # Add your variables here
+
+def calculator(first_variable, second_variable, mode)
+  when mode == add
+    puts first_variable + second_variable
+    when mode == subtract
+      puts first_variable - second_variable
+      when mode == multiply 
+        puts first_variable * second_variable
+        when mode == divide
+          puts first_variable / second_variable
+        else null
+      end
